f208f5ee8d74d6664473c7f82f252d1d782acd1f
ace857 <srjbsht857@gmail.com>
2020-06-03 19:14:40 +0530

    Initial commit

---
+++ src/main/java/com/ace/drona/DronaApplication.java
@@ -0,0 +1,13 @@
+package com.ace.drona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ace/drona/DronaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.drona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
